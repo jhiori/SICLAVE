b9dd28487c0f32d401544fb2061e2a605397dbc5
jhiori <worldhacker@hotmail.es>
2024-03-04 16:44:10 -0600

    With data

---
+++ src/app/models/entidadFederativaFull.model.ts
@@ -0,0 +1,41 @@
+export class EntidadFederativa{
+    constructor(
+        public cveEstado?: string,
+        public nameEstado?: string,
+        public cvePais?: string
+    ){
+        this.cveEstado = this.cveEstado || '';
+        this.nameEstado = this.nameEstado || '';
+        this.cvePais = this.cvePais || '';
+    }
+}
+
+
+export class Municipio{
+    constructor(
+        public cveMunicipio?: string,        
+        public cveEstado?: string,
+        public nameMunicipio?: string
+    ){
+        this.cveMunicipio = this.cveMunicipio || '';
+        this.cveEstado = this.cveEstado || '';
+        this.nameMunicipio = this.nameMunicipio || '';
+    }
+}
+
+
+export class Localidad{
+    constructor(
+        public cveLocalidad?: string,
+        public cveMunicipio?: string, 
+        public cveEstado?: string, 
+        public nameLocalidad?: string,
+          
+    ){
+        this.cveLocalidad = this.cveLocalidad || ''
+        this.cveMunicipio = this.cveMunicipio || '';
+        this.cveEstado = this.cveEstado || '';
+        this.nameLocalidad = this.nameLocalidad || ''
+        
+    }
+}
