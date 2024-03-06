a420a345337960cd430f1f7f2130617d3d9d2056
Jhiori <worldhacker@hotmail.es>
2024-03-06 17:38:58 -0600

    Login test

--- src/app/login/keys/keys.component.ts
+++ src/app/login/keys/keys.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
 
+import {
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
+import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+
 @Component({
   selector: 'app-keys',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, MatButtonModule, MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose, MatButtonModule, MatFormFieldModule, MatInputModule],
   templateUrl: './keys.component.html',
   styleUrl: './keys.component.sass'
 })
 export class KeysComponent {
+  constructor(){}
 
+  userFormControl = new FormControl('', [Validators.required, Validators.email])
+  passFormControl = new FormControl('', [Validators.required])
 }
+
+// @Component({
+//   selector:'app-keys-dialog',
+//   templateUrl: './app-keys-dialog.html',
+//   standalone: true,
+//   imports: []
+// })
+// export class DialogElementKeys{}
--- src/app/main/header/header.component.ts
+++ src/app/main/header/header.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common'
+import { KeysComponent } from '../../login/keys/keys.component';
+import { MatDialog } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.sass'
 })
 export class HeaderComponent {
 
+  constructor(    
+    private dialog: MatDialog,
+    public authService: AuthService    
+    ){}
+
+  login(resp?: any){
+    if( resp.test == 'envio')
+    this.authService.session = {
+      name: 'Jhovanny',
+      email: 'jhovanny_tellez@hacienda.gob.mx'
+    }
+    
+  }
+
+  openDialog(){
+    
+    const dialogRef = this.dialog.open(KeysComponent,{
+      width: '420px'
+    });
+    // this.dialog.open(KeysComponent);
+    
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(result)
+      this.login(result)
+    })
+
+  }
 }
